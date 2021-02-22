5efac8ad996b52230cf842c30415867194c3e321
Ricardo Pérez <ricardopp1992@gmail.com>
2021-02-22 19:28:51 -0300

    finished Hero

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import styles from '../assets/styles/index.module.css';
-
 const HeroComponent = ({ title, image }) => {
   return (
     <div className={`hero_section hero-2`}>
       <div className={`hero_content`}>
-        <h1 className="primary-title">{ title }</h1>
-        {/* <a className={`black_btn`} href="#">Get Protection</a> */}
+        <h1 className="primary-title">{title}</h1>
       </div>
-      <div className={`hero_img_wrapper`}>
-        <img className={`hero_img`} src={image} alt="hero img" />
-        <img className={`hero_pattern`} src="/img/hero-2-patter.svg" alt="hero pattern" />
-      </div>
+      <img className={`hero_img`} src={image} alt="hero img" />
+      <img className={`hero_pattern`} src="/img/hero-2-patter.svg" alt="hero pattern" />
     </div>
   );
 };
