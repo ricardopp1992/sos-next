97f52a8a5ec92eee03665148bae41706ecb1b6b2
Ricardo Pérez <ricardopp1992@gmail.com>
2021-03-12 15:15:49 -0300

    added mailgun connection

--- src/pages/api/send-message.js
+++ src/pages/api/send-message.js
@@ -1,19 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Mailgun from 'mailgun-js';
 
-
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const mg = Mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
 
 //import contactEmailGenerator from '../../lib/contactEmailGenerator';
 
 export default (req, res) => {
   const { name, surname, email, message } = JSON.parse(req.body);
-  console.log('sdasdasd')
+
   const data = {
-    from: 'contact-form@sos.com',
-    to: 'divirgiliocamila@outlook.com.ar',
-    subject: 'Contact Form of sos',
-    // html: contactEmailGenerator(`${name}`, message, email),
-    html: `${name}, ${message}, ${email}`
+    from: 'work@sosprivatesecurity.com',
+    to: 'ricardopp1992@gmail.com',
+    subject: 'Contact Form of sosprivatesecurity.com',
+    html: `
+      <h3>This is a message from sosprivatesecurity.com</h3>
+      <b>from:</b> ${name} ${surname}
+      <b>email:</b> ${email}
+
+      <p>${message}</p>
+    `
   }
-  console.log(JSON.stringify(data))
-  res.status(200).json({ response: 'OK', message: 'message sent' })
+
+  mg.messages().send(data, function (error, body) {
+    if (error || !body) {
+      console.log(error);
+      return res.status(400).end(error || 'No email sent');
+    }
+
+    return res.status(200).json(body);
+  });
 }
